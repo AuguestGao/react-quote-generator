77f08c5221ae8de18aeab3dd3fc256e58cc92227
AG <knightauguest@gmail.com>
2021-09-20 13:40:39 -0400

    added loader, font size adjusts to uts length, and fill unknown as author if missing

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ function App() {
     text: "React quote generator",
     author: "Auguest",
   });
+  const [hideLoader, toggleHideLoader] = useState(true);
 
   useEffect(() => {
     (async () => {
+      const quoteApiUrl = "https://type.fit/api/quotes";
       try {
-        const quoteApiUrl = "https://type.fit/api/quotes";
+        toggleHideLoader(false);
         const res = await fetch(quoteApiUrl);
         const data = await res.json();
         setAllQuotes(data);
+        toggleHideLoader(true);
       } catch (err) {
         console.log(err.message);
       }
@@ -25,14 +28,23 @@ function App() {
     setQuote(newQuote);
   };
 
-  return (
+  return hideLoader ? (
     <div className="quote-container">
-      <div id="quote-text"> {quote.text}</div>
-      <div id="quote-author">{quote.author}</div>
+      <div
+        className="quote-text"
+        id={`${quote.text.length > 50 ? "long-quote" : null}`}
+      >
+        {quote.text}
+      </div>
+      <div className="quote-author">
+        {quote.author ? quote.author : "Unknown"}
+      </div>
       <div className="button">
         <button onClick={getNewQuote}>New Quote</button>
       </div>
     </div>
+  ) : (
+    <div className="loader"></div>
   );
 }
 
