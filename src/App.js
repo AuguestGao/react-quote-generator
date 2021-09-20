fe10307e66686c25903f3ec6d1fdacf576b2fe00
AG <knightauguest@gmail.com>
2021-09-20 10:15:40 -0400

    added fetch and display signle quote

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [quote, setQuote] = useState({
+    text: "React quote generator",
+    author: "Auguest",
+  });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const quoteApiUrl = "https://type.fit/api/quotes";
+        const res = await fetch(quoteApiUrl);
+        const data = await res.json();
+        setAllQuotes(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    })();
+  }, []);
+
+  const getNewQuote = () => {
+    const newQuote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+    setQuote(newQuote);
+  };
+
   return (
     <div className="quote-container">
-      quote container
-      <div id="quote-text">This is the quote text.</div>
-      <div id="quote-author">Quote Author</div>
+      <div id="quote-text"> {quote.text}</div>
+      <div id="quote-author">{quote.author}</div>
       <div className="button">
-        <button>New Quote</button>
+        <button onClick={getNewQuote}>New Quote</button>
       </div>
     </div>
   );
